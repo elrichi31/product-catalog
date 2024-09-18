892d48e64d1ca3e53ceef15dcfcf379a0eb3f1a8
elrichi31 <nicolawsmoina@gmail.com>
2024-09-17 21:13:25 -0500

    Actualizar manejo de pestañas en ProductTabs

--- src/components/ProductTabs.tsx
+++ src/components/ProductTabs.tsx
@@ -35,66 +35,76 @@ interface Review {
 const ProductTabs: React.FC<ProductTabsProps> = ({ description, details, reviews, id }) => {
     const [showReviews, setShowReviews] = useState(false);
     const [reviewsData, setReviewsData] = useState<Review[]>([]);
+    const [currentTab, setCurrentTab] = useState('description'); // Estado para almacenar el tab actual
 
     const handleLoadReviews = async () => {
         const dataReviews = await getProductsReviews(id);
         setReviewsData(dataReviews.data.reviews);
         setShowReviews(true);
     };
+
+    const handleTabChange = (value: string) => {
+        setCurrentTab(value); // Actualiza el tab actual cuando cambie
+    };
+
     return (
-        <Tabs defaultValue="description" className="mt-12">
-            <TabsList>
-                <TabsTrigger value="description">Descripción</TabsTrigger>
-                <TabsTrigger value="details">Detalles</TabsTrigger>
-                <TabsTrigger value="reviews">Reseñas</TabsTrigger>
-            </TabsList>
-            <TabsContent value="description" className="mt-4">
-                <ul className="list-disc list-inside">
-                    {description.map((desc, index) => (
-                        <li key={index}>{desc}</li>
-                    ))}
-                </ul>
-            </TabsContent>
-            <TabsContent value="details" className="mt-4">
-                <ul className="list-disc list-inside">
-                    {details.map((detail, index) => (
-                        <li key={index}>{detail}</li>
-                    ))}
-                </ul>
-            </TabsContent>
-            <TabsContent value="reviews" className="mt-4">
-                {reviews.map((review, index) => (
-                    <div key={index} className="border p-4 rounded-lg mb-4">
-                        <div className="flex items-center space-x-2 mb-2">
-                            {[...Array(review.rating)].map((_, i) => (
-                                <span key={i}>⭐</span>
-                            ))}
+        <div>
+            <Tabs defaultValue="description" className="mt-12" onValueChange={handleTabChange}>
+                <TabsList className='space-x-5'>
+                    <TabsTrigger 
+                        value="description" 
+                        className={`p-1 ${currentTab === 'description' ? 'bg-blue-500 text-white' : 'bg-white-200'}`}
+                    >
+                        Descripción
+                    </TabsTrigger>
+                    <TabsTrigger 
+                        value="reviews" 
+                        className={`p-1 ${currentTab === 'reviews' ? 'bg-blue-500 text-white' : 'bg-white-200'}`}
+                    >
+                        Reseñas
+                    </TabsTrigger>
+                </TabsList>
+                <TabsContent value="description" className="mt-4">
+                    <ul className="list-disc list-inside">
+                        {description.map((desc, index) => (
+                            <li key={index}>{desc}</li>
+                        ))}
+                    </ul>
+                </TabsContent>
+                <TabsContent value="reviews" className="mt-4">
+                    {reviews.map((review, index) => (
+                        <div key={index} className="border p-4 rounded-lg mb-4">
+                            <div className="flex items-center space-x-2 mb-2">
+                                {[...Array(review.rating)].map((_, i) => (
+                                    <span key={i}>⭐</span>
+                                ))}
+                            </div>
+                            <p>{review.text}</p>
                         </div>
-                        <p>{review.text}</p>
-                    </div>
-                ))}
-                {!showReviews ? (
-                    <div className="text-center">
-                        <button
-                            onClick={handleLoadReviews}
-                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                        >
-                            Cargar reseñas de personas
-                        </button>
-                    </div>
-                ) : (
-                    <div>
-                        {
-                            reviewsData.map((review) => (
-                                <ReviewCard key={review.review_id} review={review} />
-                            ))
-                        }
-                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                            onClick={() => { setShowReviews(false) }}>Ocultar reseñas de personas</button>
-                    </div>
-                )}
-            </TabsContent>
-        </Tabs>
+                    ))}
+                    {!showReviews ? (
+                        <div className="text-center">
+                            <button
+                                onClick={handleLoadReviews}
+                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                            >
+                                Cargar reseñas de personas
+                            </button>
+                        </div>
+                    ) : (
+                        <div>
+                            {
+                                reviewsData.map((review) => (
+                                    <ReviewCard key={review.review_id} review={review} />
+                                ))
+                            }
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                                onClick={() => { setShowReviews(false) }}>Ocultar reseñas de personas</button>
+                        </div>
+                    )}
+                </TabsContent>
+            </Tabs>
+        </div>
     );
 };
 
